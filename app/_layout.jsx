22667a0b188eca349f220700785a1ed28e523b24
Mouhssine Es-Safy <myschoolccfbs@gmail.com>
2026-06-04 16:16:51 +0100

    Update the interface

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,73 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+// export default function RootLayout() {
+//   return <Stack />;
+// }
+
+// ====================================
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
+
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: true,
+        tabBarActiveTintColor: "#000",
+        tabBarInactiveTintColor: "#555",
+        tabBarLabelStyle: { fontWeight: "bold", fontSize: 12 },
+        tabBarStyle: styles.tabBar,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="individuel"
+        options={{
+          title: "Individuel",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="user" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="collectif"
+        options={{
+          title: "Collectif",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="users" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
+
+const styles = StyleSheet.create({
+  tabBar: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    right: 20,
+    backgroundColor: "#D4FF2A", // اللون الأخضر لي فالتصميم
+    borderRadius: 30,
+    height: 70,
+    paddingBottom: 10,
+    paddingTop: 10,
+    borderTopWidth: 0,
+    elevation: 10,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+  },
+});
