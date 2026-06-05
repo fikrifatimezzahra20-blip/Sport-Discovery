09d26a2e1dbc66bcb063f3e417b7e0a8bb92fbdf
Mouhssine Es-Safy <myschoolccfbs@gmail.com>
2026-06-05 15:38:37 +0100

    clear layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,3 @@
-// import { Stack } from "expo-router";
-
-// export default function RootLayout() {
-//   return <Stack />;
-// }
-
-// ====================================
 import { FontAwesome5 } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import { StyleSheet } from "react-native";
